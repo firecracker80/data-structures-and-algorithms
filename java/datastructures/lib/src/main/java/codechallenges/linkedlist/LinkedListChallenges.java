63f98e25ea9b4ae1534f1072869a6a5d4c5eff74
Yajahira <yari@yvelazquez.com>
2022-08-22 21:50:00 -0400

    added required info to readme for linkedList directory and root directory

--- java/datastructures/lib/src/main/java/codechallenges/linkedlist/LinkedListChallenges.java
+++ java/datastructures/lib/src/main/java/codechallenges/linkedlist/LinkedListChallenges.java
@@ -1,18 +1,39 @@
 package codechallenges.linkedlist;
 
 import datastructures.linkedlist.LinkedList;
+import java.util.LinkedList;
 
 public class LinkedListChallenges
 {
-  LinkedList linkedList;
+  LinkedList firstList = new LinkedList<>();
 
-  public LinkedListChallenges()
-  {
-    linkedList = new LinkedList();
+  public void insert(){
+    struct node *newNode = firstList(sizeof(struct node);
+    newNode -> data = value;
+    if(*head == null){
+		*head = newNode;
+    } else {
+      struct node *lastNode =*head;
+      while(lastNode = lastNode -> next;
+    }
+    lastNode -> next = newNode;
   }
 
-  public void linkedListChallenge06()
-  {
+firstList.addLast(&head, a);
+firstList.addLast(&head, b);
+firstList.addLast(&head, c);
+  public static includes(str){
+    firstList.contains(Object a);
 
+
+    Return firstList;
+  }
+
+Public static toString(){
+  return
   }
 }
+
+
+
+//**Retrieved from lo2base2.com
---
+++ java/datastructures/lib/src/main/java/codechallenges/linkedlist/README.md
@@ -0,0 +1,10 @@
+# Singly Linked List
+This a code challenge that requied that a linked list be created, and then populated with elements. Once these elements were added to the empty linked list, check to verify that the elements are contained within in.
+
+## Challenge
+The challenge is create the empty list, added elements, verify the elements are contained within, and finally return a string with the contained elements.
+
+## Approach & Efficiency
+I decided to research first to get an understanding and potentially find the solution to the challenge. Once the solution was found, I used it as a reference, and modified a few areas to the challenge. I have yet to figure out the toString method and testing, yet it successfully compiled without errors.
+## API
+No APIs were used for this challenge.
