eb41f75daf0263e17928aed19e9967d1be6cf3c1
Yajahira <yari@yvelazquez.com>
2022-08-23 00:03:53 -0400

    created linked list and node file again

---
+++ java/datastructures/lib/src/main/java/codechallenges/linkedlist/LinkedListChallenges.java
@@ -0,0 +1,38 @@
+package codechallenges.linkedlist;
+
+import datastructures.linkedlist.LinkedList;
+import java.util.LinkedList;
+
+public class LinkedListChallenges
+{
+  LinkedList firstList = new LinkedList<>();
+
+  public void insert(){
+    struct node *newNode = firstList(sizeof(struct node);
+    newNode -> data = value;
+    if(*head == null){
+    *head = newNode;
+    } else {
+      struct node *lastNode =*head;
+      while(lastNode = lastNode -> next;
+    }
+    lastNode -> next = newNode;
+  }
+
+firstList.addLast(&head, a);
+firstList.addLast(&head, b);
+firstList.addLast(&head, c);
+  public static includes(str){
+    firstList.contains(Object a);
+
+
+    Return firstList;
+  }
+
+Public static toString(){
+  return
+  }
+}
+
+
+//**Retrieved from lo2base2.com
